165e97c28b6ff53d21b37625f35350a0808ffc21
Erick Santana <ericksantana2009@gmail.com>
2024-10-08 18:15:29 -0300

    routes

--- src/app.js
+++ src/app.js
@@ -13,19 +13,54 @@ import {MongoClient, ObjectId} from "mongodb";
 const mongoClient = new MongoClient(URL)
 let db
 
-try{
-    await mongoClient.connect()
-    db = mongoClient.db()
-    console.log("Connected to DB")
-}
-catch(err){
-    console.log(err)
+async function connectDb (){
+    try{
+        await mongoClient.connect()
+        db = mongoClient.db()
+        console.log("Connected to DB")
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 
-app.post("/sign-up", (req, res) => {
+connectDb()
+app.use(express.json());
+
+app.post("/sign-up", async (req, res) => {
     const user = req.body
     res.send(user)
+    const result = await db.collection("users").insertOne(user)
+    console.log("Usuário inserido",result)
 })
+
+app.post("/sign-in", async (req, res) => {
+    const user = req.body
+    const result = await db.collection("users").findOne(user)
+    if (result) {
+        res.status(201).send("Logado")
+    }
+    else{
+        res.status(404).send("Usuário não encontrado")
+    }
+})
+
+app.post("/transactions", async (req,res) => {
+    const transaction = req.body
+    const result = await db.collection("transactions").insertOne(transaction)
+    if(!result){
+        res.status(400).send("Erro ao criar transação")
+    }
+    else{
+        res.status(200).send("Transação criada")
+    }
+})
+
+app.get("/transaction",async (req,res)=>{
+
+})
+
+
 
 app.listen(PORT,() => {
     console.log(`Servidor na porta ${PORT}`)
